f904ddff3b51adc2911b7a429636ac9ed7e506cf
najmul-islam <itzmenajmul@gmail.com>
2025-12-24 13:47:26 +0600

    solved Ultha Pothe Reverse challenges

---
+++ ultapothe-reverse.js
@@ -0,0 +1,21 @@
+// Practice:
+
+/*একটা names array দিয়ে নামগুলো reverse করে দেখ: const names = ['Zara', 'Tariq', 'Amir', 'Lina'];
+ */
+const names = ['Zara', 'Tariq', 'Amir', 'Lina'];
+const reverseNames = names.reverse();
+console.log("all names are reverse: ", reverseNames);
+
+/*numbers-এর array দিয়েও reverse কর: const numbers = [1, 4, 9, 7];
+ */
+const numbers = [1, 4, 9, 7];
+const numbersReverse = numbers.reverse();
+console.log("all numbers are reverse: ", numbersReverse)
+
+/*
+অবজেক্টওয়ালা অ্যারেকে reverse কর: const users = [{name: 'Ali', age: 30}, {name: 'Sara', age: 25}, {name: 'Tariq', age: 35}];
+ */
+
+const users = [{name: 'Ali', age: 30}, {name: 'Sara', age: 25}, {name: 'Tariq', age: 35}];
+const usersReverse = users.reverse();
+console.log("all the objects reverse inside array: ", usersReverse);
